a5f4938dd9e8b01530f5e8fd3de3f6d8ef877867
winston salcedo <winstonsalcedo@MacBook-Pro-4.local>
2019-05-03 15:28:26 -0600

    Initial Commit

---
+++ GitLesson/GitLesson/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitLesson
+//
+//  Created by winston salcedo on 5/3/19.
+//  Copyright © 2019 Evolve Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
